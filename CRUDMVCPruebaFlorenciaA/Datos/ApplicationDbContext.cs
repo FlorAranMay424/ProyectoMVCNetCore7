fce242374fa7a936055217a86a65dd87e5af4e59
FlorAranMay2024 <arancibiaflorprogramacion@gmail.com>
2024-06-12 17:11:11 -0300

    Primera subida del Proyecto MVC Net Core 7

---
+++ CRUDMVCPruebaFlorenciaA/Datos/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using CRUDMVCPruebaFlorenciaA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDMVCPruebaFlorenciaA.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        
+        }
+
+        //Agregar los modelos aqui
+        public DbSet<Contacto> Contacto { get; set; }
+    
+            
+
+
+    }
+}
