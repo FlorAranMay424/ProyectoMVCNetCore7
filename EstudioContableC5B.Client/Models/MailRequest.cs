0ff00a2a4aaf374dc968b2dd8a4991da795489af
FlorAranMay2024 <arancibiaflorprogramacion@gmail.com>
2024-06-15 17:24:58 -0300

    Cambios en contacto, pero subo todo porque no encontré la carpeta que tenía el repo, así que volví a copiar todo y lo vuelvo a subir.

---
+++ EstudioContableC5B.Client/Models/MailRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace EstudioContableC5B.Client.Models
+{
+    public class MailRequest
+    {
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; } 
+        public string NombreUsuario { get; set; }
+        public string Direccion { get; set; }
+        public string Direccion2 { get; set; }
+
+        public string Pais { get; set; }
+        public string Provincia { get; set; }
+
+        public string Localidad { get; set; }
+
+        public string Cuit { get; set; }
+        public string Password { get; set;}
+
+        public string Tramite { get; set; }
+
+
+    }
+}
---
+++ EstudioContableC5B.Client/Services/IEmailSenderService.cs
@@ -0,0 +1,11 @@
+﻿using EstudioContableC5B.Client.Models;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+namespace EstudioContableC5B.Client.Services
+{
+    public interface IEmailSenderService
+    {
+        Task SendEmailAsync(MailRequest request);
+    }
+}
